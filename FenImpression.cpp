afc15ff16d8b732294c05357e6e2b383c50e9e25
MN PO <myna65@hotmail.com>
2012-08-09 12:10:09 +0200

    Ajout d'un système de sauvegarde des options pour l'impression centralisée

--- FenImpression.cpp
+++ FenImpression.cpp
@@ -1,5 +1,6 @@
 #include "FenImpression.h"
 #include "ui_Dialogimpression.h"
+#include <QtCore/qsettings.h>
 struct Totaux
 {
     bool quot;
@@ -39,14 +40,32 @@ FenImpression::FenImpression(QWidget *parent) :
     ui(new Ui::DialogImpression)
 {
     ui->setupUi(this);
-    ui->dateEdit->setDate(QDate::currentDate());
-    ui->dateEdit_2->setDate(QDate::currentDate());
     connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(imprimer()));
     connect(ui->checkBox_5,SIGNAL(clicked()),this,SLOT(toupdate()));
     connect(ui->checkBox_6,SIGNAL(clicked()),this,SLOT(toupdate()));
     connect(ui->checkBox_7,SIGNAL(clicked()),this,SLOT(toupdate()));
     connect(ui->checkBox_8,SIGNAL(clicked()),this,SLOT(toupdate()));
     connect(ui->checkBox_9,SIGNAL(clicked()),this,SLOT(toupdate()));
+    QSettings settings("Gestion","Elixir");
+    settings.beginGroup("Impression/Centralisee");
+    ui->dateEdit->setDate(settings.value("Dateg",QDate::currentDate()).toDate());
+    ui->dateEdit_2->setDate(settings.value("Dated",QDate::currentDate()).toDate());
+    ui->checkBox->setChecked(settings.value("1",false).toBool());
+    ui->checkBox_2->setChecked(settings.value("2",false).toBool());
+    ui->checkBox_3->setChecked(settings.value("3",false).toBool());
+    ui->checkBox_4->setChecked(settings.value("4",false).toBool());
+    ui->checkBox_5->setChecked(settings.value("5",false).toBool());
+    ui->checkBox_6->setChecked(settings.value("6",false).toBool());
+    ui->checkBox_7->setChecked(settings.value("7",false).toBool());
+    ui->checkBox_8->setChecked(settings.value("8",false).toBool());
+    ui->checkBox_9->setChecked(settings.value("9",false).toBool());
+    ui->checkBox_10->setChecked(settings.value("10",false).toBool());
+    ui->checkBox_11->setChecked(settings.value("11",false).toBool());
+    ui->checkBox_12->setChecked(settings.value("12",false).toBool());
+    ui->checkBox_13->setChecked(settings.value("13",false).toBool());
+    ui->checkBox_14->setChecked(settings.value("14",false).toBool());
+    ui->checkBox_15->setChecked(settings.value("15",false).toBool());
+    ui->checkBox_16->setChecked(settings.value("16",false).toBool());
     toupdate();
 }
 
@@ -106,6 +125,26 @@ void FenImpression::imprimer()
         opt.recap.totaux.actif=true;
         opt.recap.totaux.caisse=ui->checkBox_13->isChecked();
     }
+    QSettings settings("Gestion","Elixir");
+    settings.beginGroup("Impression/Centralisee");
+    settings.setValue("1",ui->checkBox->isChecked());
+    settings.setValue("2",ui->checkBox_2->isChecked());
+    settings.setValue("3",ui->checkBox_3->isChecked());
+    settings.setValue("4",ui->checkBox_4->isChecked());
+    settings.setValue("5",ui->checkBox_5->isChecked());
+    settings.setValue("6",ui->checkBox_6->isChecked());
+    settings.setValue("7",ui->checkBox_7->isChecked());
+    settings.setValue("8",ui->checkBox_8->isChecked());
+    settings.setValue("9",ui->checkBox_9->isChecked());
+    settings.setValue("10",ui->checkBox_10->isChecked());
+    settings.setValue("11",ui->checkBox_11->isChecked());
+    settings.setValue("12",ui->checkBox_12->isChecked());
+    settings.setValue("13",ui->checkBox_13->isChecked());
+    settings.setValue("14",ui->checkBox_14->isChecked());
+    settings.setValue("15",ui->checkBox_15->isChecked());
+    settings.setValue("16",ui->checkBox_16->isChecked());
+    settings.setValue("Dateg",ui->dateEdit->date());
+    settings.setValue("Dated",ui->dateEdit_2->date());
 }
 void FenImpression::toupdate()
 {
